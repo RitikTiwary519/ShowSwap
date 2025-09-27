ba7c97702bf81aa18790e3c7654bcc2a2ed912ee
RitikTiwary519 <202201519@daiict.ac.in>
2025-09-27 19:10:28 +0530

    website down

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
 import Header from './components/Header';
+import SiteDown from './pages/SiteDown';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import PostTicketPage from './pages/PostTicketPage';
@@ -15,23 +16,7 @@ const App: React.FC = () => {
         <AuthProvider>
             <HashRouter>
                 <div className="min-h-screen flex flex-col bg-dark-bg font-sans">
-                    <Header />
-                    <main className="container mx-auto p-4 md:p-8 flex-1">
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/post" element={
-                                <ProtectedRoute>
-                                    <PostTicketPage />
-                                </ProtectedRoute>
-                            } />
-                            <Route path="/admin" element={
-                                <ProtectedRoute adminOnly={true} adminUids={ADMIN_UIDS}>
-                                    <AdminPage />
-                                </ProtectedRoute>
-                            } />
-                        </Routes>
-                    </main>
-                    <Footer />
+                    <SiteDown />
                 </div>
             </HashRouter>
         </AuthProvider>
---
+++ pages/SiteDown.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SiteDown: React.FC = () => (
+  <div className="min-h-screen flex flex-col items-center justify-center bg-dark-bg text-white">
+    <h1 className="text-4xl font-bold mb-4">Website is Down</h1>
+    <p className="text-lg mb-2">Thanks for showing interest.</p>
+    <p className="text-md">We will get back to you soon.</p>
+  </div>
+);
+
+export default SiteDown;
